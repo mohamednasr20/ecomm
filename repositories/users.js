06919fb5d79f23605d2dc50cc8c8b5fdab8b12ad
mohamednasr20 <mohamednasr8687@gmail.com>
2021-02-02 10:07:03 -0500

    add costum database

---
+++ repositories/users.js
@@ -0,0 +1,32 @@
+const { json } = require("express");
+const fs = require("fs");
+
+class UsersRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error("creating a repository requires a filename.");
+    }
+
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, "[]");
+    }
+  }
+
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, { encoding: "utf8" })
+    );
+  }
+}
+
+const test = async () => {
+  const repo = new UsersRepository("users.json");
+  const users = await repo.getAll();
+
+  console.log(users);
+};
+
+test();
