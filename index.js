050a3926ad575ae5abcdf04aea5293d161fecd5b
mohamednasr20 <mohamednasr8687@gmail.com>
2021-02-03 00:19:54 -0500

    add form validation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const usersRepo = require("./repositories/users");
 
 const app = express();
 
@@ -10,15 +11,25 @@ app.get("/", (req, res) => {
     <form method="POST">
      <input placeholder="Email" name="email" type="email"/>
      <input placeholder="Password" name="password" type="password"/>
-     <input placeholder="Confirm Password" name="passwordComfrmation"    type="password"/>
+     <input placeholder="Confirm Password" name="passwordConfirmation"    type="password"/>
      <button>Sign Up</button>
     </form>
   `);
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("acount created ...");
+app.post("/", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUsers = await usersRepo.getOneBy({ email });
+  if (existingUsers) {
+    return res.send("Email In Use");
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send("passwords must match");
+  }
+
+  res.send("Account Created!!!");
 });
 
 app.listen(3000, () => {
--- repositories/users.js
+++ repositories/users.js
@@ -80,12 +80,4 @@ class UsersRepository {
   }
 }
 
-const test = async () => {
-  const repo = new UsersRepository("users.json");
-
-  const user = await repo.getOneBy({ email: "anything@gmail.com" });
-
-  console.log(user);
-};
-
-test();
+module.exports = new UsersRepository("users.json");
