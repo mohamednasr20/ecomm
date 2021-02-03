1325ed19e26871cffba198d851ea5ddcfcf2a090
mohamednasr20 <mohamednasr8687@gmail.com>
2021-02-02 22:47:50 -0500

    add create records method

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,6 @@
 const { json } = require("express");
 const fs = require("fs");
+const { stringify } = require("querystring");
 
 class UsersRepository {
   constructor(filename) {
@@ -20,10 +21,24 @@ class UsersRepository {
       await fs.promises.readFile(this.filename, { encoding: "utf8" })
     );
   }
+
+  async create(attrs) {
+    const recordes = await this.getAll();
+    recordes.push(attrs);
+    await this.writeAll(recordes);
+  }
+
+  async writeAll(recordes) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(recordes, null, 2)
+    );
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
+  await repo.create({ email: "mohamed2", password: "1234" });
   const users = await repo.getAll();
 
   console.log(users);
