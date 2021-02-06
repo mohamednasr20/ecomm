15e94520298e134771e00c919db7f8b48683fe40
mohamednasr20 <mohamednasr8687@gmail.com>
2021-02-06 00:15:23 -0500

    refactor gerError function

--- views/admin/signin.js
+++ views/admin/signin.js
@@ -1,11 +1,14 @@
 const layout = require("./layout");
+const { getError } = require("../helpers");
 
-module.exports = () => {
+module.exports = ({ errors }) => {
   return layout({
     content: `
     <form method="POST">
       <input placeholder="Email" name="email" type="email"/>
+      ${getError(errors, "email")}
       <input placeholder="Password" name="password" type="password"/>
+      ${getError(errors, "password")}
       <button>Sign In</button>
    </form>
     `,
