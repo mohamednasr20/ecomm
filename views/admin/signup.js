a798fe887b6f9c68118079c208b188f9a7a99353
mohamednasr20 <mohamednasr8687@gmail.com>
2021-02-05 21:18:03 -0500

    add errors msg to signup form

--- views/admin/signup.js
+++ views/admin/signup.js
@@ -1,14 +1,25 @@
 const layout = require("./layout");
 
-module.exports = ({ req }) => {
+const getError = (errors, prop) => {
+  try {
+    return errors.mapped()[prop].msg;
+  } catch (err) {
+    return "";
+  }
+};
+
+module.exports = ({ req, errors }) => {
   return layout({
     content: `
     <div>
       Your id is: ${req.session.userId}
       <form method="POST">
         <input placeholder="Email" name="email" type="email"/>
+        ${getError(errors, "email")}
         <input placeholder="Password" name="password" type="password"/>
+        ${getError(errors, "password")}
         <input placeholder="Confirm Password" name="passwordConfirmation"    type="password"/>
+        ${getError(errors, "passwordConfirmation")}
         <button>Sign Up</button>
       </form>
     </div>
